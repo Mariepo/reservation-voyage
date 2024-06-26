1002846932fbb753a013edba9e39d51053ceba7d
Mariepo <34646836+Mariepo@users.noreply.github.com>
2024-06-26 11:58:19 +0200

    Revert "admin - modifier une destination + alerte de confirmation"

--- models/admin/destinations-model.php
+++ models/admin/destinations-model.php
@@ -5,31 +5,15 @@ require_once "bdd.php";
 //     // récupérer les destinations
 function fetchDestinations(){
     global $pdo; // Ajoutez cette ligne pour rendre la variable $pdo accessible dans la fonction
-    $query = "SELECT id_destination, nom, description, prix FROM destination;";
+    $query = "SELECT nom, description, prix FROM destination;";
     $stmt = $pdo->prepare($query);
     $stmt->execute();
     return $stmt->fetchAll(PDO::FETCH_ASSOC); 
 };
 
-// Afficher destinations
 function insertDestination($nom, $description, $prix, $disponibilite){
     global $pdo; // Ajoutez cette ligne pour rendre la variable $pdo accessible dans la fonction
     $query = "INSERT INTO destination (nom, description, prix, disponibilite) VALUES (?, ?, ?, ?)";
     $stmt = $pdo->prepare($query);
     $stmt->execute([$nom, $description, $prix, $disponibilite]);
-};
-
-function fetchDestinationById($id_destination){
-    global $pdo; // Ajoutez cette ligne pour rendre la variable $pdo accessible dans la fonction
-    $query = "SELECT id_destination, nom, description, prix, disponibilite FROM destination WHERE id_destination = ?;";
-    $stmt = $pdo->prepare($query);
-    $stmt->execute([$id_destination]);
-    return $stmt->fetch(PDO::FETCH_ASSOC); 
-}
-
-function updateDestination($id_destination, $nom, $description, $prix, $disponibilite){
-    global $pdo;
-    $query = "UPDATE destination SET nom = ?, description = ?, prix = ?, disponibilite = ? WHERE id_destination = ?;";
-    $stmt = $pdo->prepare($query);
-    $stmt->execute([$nom, $description, $prix, $disponibilite, $id_destination]);
 };
--- views/admin/destinations/index.php
+++ views/admin/destinations/index.php
@@ -1,72 +1,45 @@
 
-<?php
-    // Si ajout de destination
-    if (isset($_GET["create"]) && $_GET["create"] == "success") {
-        echo '<div style="background-color: green; padding: 12px" class="banner">';
-            echo '<span style="color:white;">Destination ajoutée avec succès !</span>';
-            echo '<button class="close-btn">X</button>';
-        echo '</div>';
-    }; 
+<button style="background-color: lightblue;">Destinations</button>
+<button>Reservations</button>
+<button>Clients</button>
+<button>Paiement</button>
 
-    // Si modification de destination
-    if(isset($_GET["update"]) && $_GET["update"] == "success"){
-        echo '<div style="background-color: green; padding: 12px" class="banner">';
-            echo '<span style="color:white;">Destination modifiée avec succès !</span>';
-            echo '<button class="close-btn">X</button>';
-        echo '</div>'; 
-    };
-?>
-
-<main>
-    <button style="background-color: lightblue;">Destinations</button>
-    <button>Réservations</button>
-    <button>Clients</button>
-    <button>Paiements</button>
-
-    <h1>Destinations</h1>
-    <div>
-        <button onclick="redirectToCreateDestination()">Ajouter une destination</button>
-    </div>
-    <table>
-        <tr>
-            <th>Nom</th>
-            <th>Description</th>
-            <th>Prix</th>
-            <th>Actions</th>
-        </tr>
-        <tr>
-            <?php
-                foreach($destinations as $destination){
-                    echo "<tr>";
-                        echo "<td>" . htmlspecialchars($destination["nom"]) . "</td>";
-                        echo "<td>" . htmlspecialchars($destination["description"]) . "</td>";
-                        echo "<td>" . htmlspecialchars($destination["prix"]) . "</td>";
-                        echo "<td>";
-                        echo "<button onclick='redirectToEditDestination(" . htmlspecialchars($destination['id_destination']) .")'>Modifier</button>";
-                        echo "<button>Supprimer</button>";
-                        echo "</td>";
-                    echo "</tr>";
-                };
-            ?>
-        </tr>
-    </table>
-</main>
+<h1>Destinations</h1>
+<div>
+    <button onclick="redirectToCreateDestination()">Ajouter une destination</button>
+</div>
+<table>
+    <tr>
+        <th>Nom</th>
+        <th>Description</th>
+        <th>Prix</th>
+        <th>Actions</th>
+    </tr>
+    <tr>
+        <?php
+            foreach($destinations as $destination){
+                echo "<tr>";
+                    echo "<td>";
+                    echo htmlspecialchars($destination["nom"]) . "<br>";
+                    echo "</td>";
+                    echo "<td>";
+                    echo htmlspecialchars($destination["description"]) . "<br>";
+                    echo "</td>";
+                    echo "<td>";
+                    echo htmlspecialchars($destination["prix"]) . "<br>";
+                    echo "</td>";
+                    echo "<td>";
+                    echo "<button>Modifier</button>";
+                    echo "<button>Supprimer</button>";
+                    echo "</td>";
+                echo "</tr>";
+            };
+        ?>
+    </tr>
+</table>
 
 <script type="text/javascript">
     function redirectToCreateDestination(){
         window.location.replace("../../controllers/admin/destinations-controller.php?action=create");
     }
-    function redirectToEditDestination(id_destination){
-        window.location.replace(`../../controllers/admin/destinations-controller.php?action=edit&id_destination=${id_destination}`);
-    }
-    function closeBanner(){
-        const banner = document.querySelector(".banner");
-        const closeBtn = document.querySelector(".close-btn");
-        if(banner) {
-            closeBtn.addEventListener("click", function(){
-                banner.style.display = "none";
-            })
-        }
-    }
-    closeBanner();
 </script>
